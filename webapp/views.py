6d676feb43deffa10574279e1e60a784bded736d
fredboys <freddieboys@hotmail.co.uk>
2022-12-16 18:25:59 +0000

    Add url.py file and create base template

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Room
 
-# Create your views here.
+
+class Home(generic.TemplateView):
+
+    template_name = 'index.html'
