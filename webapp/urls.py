591fdb9a73eba059a86530ba069516cc521a2a60
fredboys <freddieboys@hotmail.co.uk>
2022-12-20 18:15:45 +0000

    Update admin panel with all rooms and start room html file

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import Home, Rooms
 
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('rooms/', Rooms.as_view(), name='room'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -7,3 +7,13 @@ from .models import Room
 class Home(generic.TemplateView):
 
     template_name = 'index.html'
+
+
+class Rooms(generic.ListView):
+    '''
+    Display treatments images, title, descriptions and prices for the website
+    '''
+    model = Room
+
+    queryset = Room.objects.all()
+    template_name = 'room.html'
