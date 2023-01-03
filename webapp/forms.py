ac47b525f754fd477dcdd899567a43d5252d77db
fredboys <freddieboys@hotmail.co.uk>
2023-01-03 12:59:08 +0000

    Added and started styling to book.html

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,9 +1,20 @@
-from .models import Booking
+from .models import Booking, Room
 from django import forms
 import datetime
 
 
 class BookingForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(BookingForm, self).__init__(*args, **kwargs)
+        rooms = Room.objects.filter().order_by("name").values()
+        room_tuples = [("", "----------")]
+        room_tuples = room_tuples + [(str(
+            i["id"]) + "," + str(
+                i["duration"]), i["name"] + " - " + "£" + str(i["price"])) for i in rooms]
+        self.fields['room_name'] = forms.ChoiceField(choices=room_tuples)
+
+
     class Meta:
         model = Booking
         fields = ('first_name', 'last_name', 'email',  'phone',
