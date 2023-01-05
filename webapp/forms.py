2d73f8a650eb668b20d31bab001ebd551f4ab07d
fredboys <freddieboys@hotmail.co.uk>
2023-01-05 11:00:23 +0000

    Working on time picker

--- webapp/forms.py
+++ webapp/forms.py
@@ -8,7 +8,7 @@ class DateInput(forms.DateInput):
 
 
 class TimeInput(forms.TimeInput):
-    input_type = 'text'
+    input_type = 'time'
 
 
 class BookingForm(forms.ModelForm):
@@ -21,19 +21,7 @@ class BookingForm(forms.ModelForm):
                 'min': datetime.date.today()+datetime.timedelta(days=0),
                 'max': datetime.date.today()+datetime.timedelta(days=30)}),
             'time': TimeInput(attrs={
-                'class': 'timepicker'}),
-        }
-
-
-class EditBookingForm(forms.ModelForm):
-
-    class Meta:
-        model = Booking
-        fields = ('first_name', 'last_name', 'email', 'phone', 'date', 'time', 'room_name')
-        widgets = {
-            'date': DateInput(attrs={
-                'min': datetime.date.today()+datetime.timedelta(days=0),
-                'max': datetime.date.today()+datetime.timedelta(days=30)}),
-            'time': TimeInput(attrs={
-                'class': 'timepicker'}),
+                'min': "10:00",
+                'max': "18:00",
+                'step': "600"}),
         }
