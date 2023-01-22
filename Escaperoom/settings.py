7431ef68b340546cd1292211eb26a17e644523fd
fredboys <freddieboys@hotmail.co.uk>
2023-01-22 10:19:54 +0000

    moved email username to env.py

--- Escaperoom/settings.py
+++ Escaperoom/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 
@@ -170,6 +170,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'theescaperoomldn@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USERNAME')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'theescaperoomldn@gmail.com'
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_USERNAME')
+
